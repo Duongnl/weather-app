2918204a98dda195539c715d63b63774c60cd8a7
Duongg <lengocduong003@gmail.com>
2025-04-21 15:29:12 +0700

    init fragment

--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -7,18 +7,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Weather Map"));
+
+        // Mặc định hiển thị fragment đầu tiên
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, new HomeFragment())
+                .commit();
+
+
+        // Lắng nghe sự kiện tab được chọn
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment selectedFragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case 1:
+                        selectedFragment = new WeatherMapFragment();
+                        break;
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragmentContainer, selectedFragment)
+                            .commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Không cần xử lý nếu không cần lưu trạng thái tab cũ
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Có thể dùng để refresh lại fragment
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
 }
